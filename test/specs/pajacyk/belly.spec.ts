afb8a56a6100555b681d303d1ab6cb1e64d0f357
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-02-06 23:26:38 +0100

    pajacyk test case

---
+++ test/specs/pajacyk/belly.spec.ts
@@ -0,0 +1,9 @@
+import HomePage from '../../pageobjects/pajacyk/home.page';
+
+describe("Pajacyk", () => {
+    it("belly click should donate kids", async () => {
+        await HomePage.open();
+        await HomePage.clickBelly();
+        await expect(HomePage.thanks).toBeDisplayed();
+    });
+});
